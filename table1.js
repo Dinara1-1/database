446b6ae634541f4cca6503f3a231076f17e0c0fa
Dinara1-1 <140155118+Dinara1-1@users.noreply.github.com>
2023-08-14 19:21:04 +0400

    Update table1.js

--- table1.js
+++ table1.js
@@ -1,6 +1,6 @@
-let recipeList1 = JSON.parse(localStorage.getItem("recipeList"));
+let recipeList = JSON.parse(localStorage.getItem("recipeList"));
 
-let recipeList = [
+let recList = [
     {
       id: 1,
       name: "Омлет",
